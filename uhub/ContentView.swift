af75406f8b5eb435e15e9823a659c4daea14c2f8
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-08-30 18:07:07 +0700

    Initial Commit

---
+++ uhub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uhub
+//
+//  Created by Truong Nhat Anh on 30/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uhub/uhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  uhubApp.swift
+//  uhub
+//
+//  Created by Truong Nhat Anh on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct uhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
