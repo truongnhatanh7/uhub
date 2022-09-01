091a7ba89ccffc18886456b5e26f0a17b9098365
whisky-lover <s3877698@rmit.edu.vn>
2022-09-01 11:12:15 +0700

    2. Bao: feat: Congrats View

---
+++ uhub/TempHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  TempHomeView.swift
+//  uhub
+//
+//  Created by Quoc Bao Nguyen Luu on 01/09/2022.
+//
+
+import SwiftUI
+
+struct TempHomeView: View {
+    var body: some View {
+        Text("Temp Home View")
+    }
+}
+
+struct TempHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        TempHomeView()
+    }
+}
