008835d1ab40eabaa7df043ff4b7bc79725a163a
Nguyen Luu Quoc Bao <s3877698@rmit.edu.vn>
2022-09-07 15:50:32 +0700

    Bao. fix:style to global style, remove scale effect

--- uhub/Style_GlobalStyle.swift
+++ uhub/Style_GlobalStyle.swift
@@ -50,3 +50,33 @@ struct OneThirdModalStyle: ViewModifier {
             .transition(.move(edge: .bottom))
     }
 }
+
+struct LogoSplashScreenModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.scaledToFit().frame(minWidth: 250, idealWidth: 280, maxWidth: 300).padding()
+    }
+}
+
+struct CongratsLogoStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content.scaledToFit().frame(minWidth: 170, idealWidth: 180, maxWidth: 190, minHeight: 180, idealHeight: 186, maxHeight: 190)
+    }
+}
+
+struct CongratsTextStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content.multilineTextAlignment(TextAlignment.center).lineLimit(3).foregroundColor(Color("pink_primary")).lineSpacing(10)
+    }
+}
+
+struct CongratsButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content.frame(minWidth: 285, idealWidth: 292, maxWidth: 300, minHeight: 50, idealHeight: 55, maxHeight: 60).background(LinearGradient(gradient: Gradient(colors: [Color("pink_disabled"), Color("pink_primary")]), startPoint: .topLeading, endPoint: .bottomTrailing)).foregroundColor(.white).cornerRadius(32)
+    }
+}
+
+struct ModalStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content.frame(minWidth: 320, idealWidth: 350, maxWidth: 360, minHeight: 450, idealHeight: 460, maxHeight: 480).background(.white).cornerRadius(24).padding()
+    }
+}
--- uhub/View_Splash.swift
+++ uhub/View_Splash.swift
@@ -11,21 +11,19 @@ struct SplashView: View {
     @EnvironmentObject var pageVM: PageViewModel
     @State var isActive:Bool = false
     @State var isShowAnimation:Bool = false
-    @State var scaleValue = CGFloat(0.75)
+   
     
     var body: some View {
         VStack {
-            Image("Logo").resizable().modifier(LogoSplashScreenModifier()).opacity(isShowAnimation ? 1.0 : 0.0).scaleEffect(scaleValue).animation(Animation.linear(duration: 2.0), value: isShowAnimation)
+            Image("Logo").resizable().modifier(LogoSplashScreenModifier()).opacity(isShowAnimation ? 1.0 : 0.0).animation(Animation.linear(duration: 2.0), value: isShowAnimation)
             
         }.onAppear {
             
-            // load necessary data here
-            
             isShowAnimation = true
-            scaleValue = 1.0
             DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
                 pageVM.visit(page: .SignUp)
             }
+            
         }
     }
 }
