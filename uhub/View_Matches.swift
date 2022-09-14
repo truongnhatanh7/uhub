b9cfa8d8fb5dc1b17c268a7ab9e8cd29d910e2ee
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-09-14 22:48:03 +0700

    feat: add mock grid view

--- uhub/View_Matches.swift
+++ uhub/View_Matches.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 struct MatchesView: View {
     @State var showMenu = false
+    private var data = Array(1...20)
 
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 120))
+    ]
+    
     var body: some View {
         VStack {
+            GeometryReader  { geometry in
+                ScrollView {
+                    HeaderHome(title: "Matches")
+                    LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+                        ForEach(data, id: \.self) { number in
+                            ZStack {
+                                Card(image: Image("User4"), width: geometry.size.width / 2.2, height: geometry.size.height / 3)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
             Spacer()
             if showMenu {
                 BottomBar {
