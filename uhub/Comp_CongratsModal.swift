209ae57ef9f273c5082f60aeb8c4f946b41870ca
Andrew Le <s3820098@rmit.edu.vn>
2022-09-17 20:11:26 +0700

    24. Andrew: update file headers

--- uhub/Comp_CongratsModal.swift
+++ uhub/Comp_CongratsModal.swift
@@ -3,12 +3,16 @@
  Course: COSC2659 iOS Development
  Semester: 2022B
  Assessment: Assignment 3
- Author: Nguyen Luu Quoc Bao
- ID: s3877698
- Created  date: 01/09/2022
- Last modified: 01/09/2022
- Acknowledgement: None
- */
+ Authors:
+ + S3877980 - Ho Le Minh Thach
+ + S3878231 - Truong Nhat Anh
+ + S3877698 - Nguyen Luu Quoc Bao
+ + S3820098  - Le Nguyen Truong An
+ Created  date: 17/09/2022
+ Last modified: 17/09/2022
+ Acknowledgements: Learning from Hacking with Swift to implement MVVM, and the usage of CoreData
+ Hudson, P. (n.d.). The 100 days of Swiftui. Hacking with Swift. Retrieved July 30, 2022, from https://www.hackingwithswift.com/100/swiftui
+*/
 
 import SwiftUI
 
--- uhub/View_Notification.swift
+++ uhub/View_Notification.swift
@@ -1,9 +1,18 @@
-//
-//  NotificationView.swift
-//  uhub
-//
-//  Created by Quoc Bao Nguyen Luu on 01/09/2022.
-//
+/*
+ RMIT University Vietnam
+ Course: COSC2659 iOS Development
+ Semester: 2022B
+ Assessment: Assignment 3
+ Authors:
+ + S3877980 - Ho Le Minh Thach
+ + S3878231 - Truong Nhat Anh
+ + S3877698 - Nguyen Luu Quoc Bao
+ + S3820098  - Le Nguyen Truong An
+ Created  date: 17/09/2022
+ Last modified: 17/09/2022
+ Acknowledgements: Learning from Hacking with Swift to implement MVVM, and the usage of CoreData
+ Hudson, P. (n.d.). The 100 days of Swiftui. Hacking with Swift. Retrieved July 30, 2022, from https://www.hackingwithswift.com/100/swiftui
+*/
 
 import SwiftUI
 import MediaPlayer
